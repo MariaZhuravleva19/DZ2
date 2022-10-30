4f0763459ecfdf7609dac38756fa7fe1ee4c6ae5
MariaZhuravleva19 <115718896+MariaZhuravleva19@users.noreply.github.com>
2022-10-30 19:57:07 +0500

    Create zadanie1.cpp

---
+++ zadanie1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	for (int i = 0; i < 1000; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			a += i;
+
+		}
+
+
+	}
+	 
+	cout << a << endl;
